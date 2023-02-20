ebc74684d55f4edcebbd08a7270611614ed043b6
gdumani <gdumani@gmail.com>
2023-02-19 21:32:13 -0600

    add style to book details

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -9,38 +9,41 @@ const Book = () => {
   const image = `https://www.gutenberg.org/cache/epub/${id}/pg${id}.cover.medium.jpg`;
 
   return (
-    <div>
+    <div className="main">
       <h2>{book.title}</h2>
       <img src={image} alt="book.name" />
-      <div>
-        {book.authors.map((author, i) => {
-          const key = i;
-          return <h4 key={key}>{author.name}</h4>;
-        })}
+      <div className="details-grid">
+
+        <div className="details-item">
+          {book.authors.map((author, i) => {
+            const key = i;
+            return <h4 key={key}>{author.name}</h4>;
+          })}
+        </div>
+        <div className="details-item">
+          {book.subjects.map((topic, i) => {
+            const key = i;
+            return <p key={key}>{topic}</p>;
+          })}
+        </div>
+        <div className="details-item">
+          {book.languages.map((lang, i) => {
+            const key = i;
+            return (
+              <span key={key}>
+                {lang}
+                {' '}
+              </span>
+            );
+          })}
+        </div>
+        <p className="details-item">
+          {book.download_count}
+          {' '}
+          downloads
+        </p>
+        <a href={book.formats['text/html']} target="_blank" rel="noreferrer">Go read it</a>
       </div>
-      <div>
-        {book.subjects.map((topic, i) => {
-          const key = i;
-          return <p key={key}>{topic}</p>;
-        })}
-      </div>
-      <div>
-        {book.languages.map((lang, i) => {
-          const key = i;
-          return (
-            <span key={key}>
-              {lang}
-              {' '}
-            </span>
-          );
-        })}
-      </div>
-      <p>
-        {book.download_count}
-        {' '}
-        downloads
-      </p>
-      <a href={book.formats['text/html']} target="_blank" rel="noreferrer">Go read it</a>
     </div>
 
   );
